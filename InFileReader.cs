44aa53014aafe11a75b72e81583a210a8c07d4c4
stefsonsampaio <stefsonsampaio01@gmail.com>
2024-08-31 21:43:15 -0300

    Commit inicial: InFileReader pegando informações de arquivo de configuração .ini

---
+++ InFileReader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgentSqlMonitor
+{
+    public class IniFileReader
+    {
+        private readonly string _filePath;
+
+        public IniFileReader(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        private static extern int GetPrivateProfileString(string section, string key, string defaultValue, StringBuilder returnValue, int size, string filePath);
+
+        public string Read(string section, string key, string defaultValue = "")
+        {
+            var returnValue = new StringBuilder(255);
+            GetPrivateProfileString(section, key, defaultValue, returnValue, 255, _filePath);
+            return returnValue.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AgentSqlMonitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string iniFilePath = @"C:\TESTE.INI";
+            var iniFileReader = new IniFileReader(iniFilePath);
+
+            string serverName = iniFileReader.Read("DATABASE", "servername");
+            string databaseName = iniFileReader.Read("DATABASE", "database");
+
+            Console.WriteLine($"Server Name: {serverName}");
+            Console.WriteLine($"Database Name: {databaseName}");
+        }
+    }
+}
